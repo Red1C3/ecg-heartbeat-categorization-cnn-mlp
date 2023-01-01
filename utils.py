555cddfcb7ab79283b608359794ea13178d9a558
Mohammad Issawi <2000issawi@gmail.com>
2023-01-01 20:46:54 +0300

    Add general import function

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import tensorflow.keras.layers as layers
 from sklearn.metrics import classification_report
 import numpy as np
 from tensorflow.keras.regularizers import l2,l1,l1_l2
+from sklearn.model_selection import train_test_split
 from scipy import signal
 from scipy.interpolate import interp1d
 
@@ -327,3 +328,54 @@ def importTrainingSet_nn(oversampling=False,binary_set=False):
     x = x.to_numpy()
 
     return x, y
+
+def import_set(oversampling,binary_set):
+    if binary_set==False :
+      dataframe = pd.read_csv('./mitbih_train.csv', header=None)
+      dataframe_test = pd.read_csv('./mitbih_test.csv', header=None)
+      y_test = dataframe_test[dataframe_test.columns[-1:]]
+      x_test = dataframe_test[dataframe_test.columns[:-1]]
+      y_test = y_test.to_numpy()
+      x_test = x_test.to_numpy()
+    else:
+      normal=pd.read_csv('./ptbdb_normal.csv',header=None)
+      abnormal=pd.read_csv('./ptbdb_abnormal.csv',header=None)
+      dataframe=pd.concat([normal,abnormal],axis=0)
+      dataframe=dataframe.sample(frac=1)
+      x,x_test,y,y_test=train_test_split(x,y,test_size=0.2)
+      dataframe=pd.concat([x,y],axis=1)
+      x_test=x_test.to_numpy()
+      y_test=y_test.to_numpy()
+
+
+    if oversampling:        
+        max_cat_size=max(len(dataframe[dataframe[187]==0]),
+                         len(dataframe[dataframe[187]==1]),
+                         len(dataframe[dataframe[187]==2]),
+                         len(dataframe[dataframe[187]==3]),
+                         len(dataframe[dataframe[187]==4]))
+        j=2
+        def resample_signal(row):
+          resampledRow=signal.resample(row[:-1],187*j)
+          f=interp1d(np.linspace(0,187,num=187*j,endpoint=False),resampledRow)
+          row[:-1]=f(np.linspace(0.5,187,num=187,endpoint=False))
+          return row
+        
+        def oversample(group):
+            samples_ratio=(max_cat_size//len(group))-1
+            group_copy=group.copy()
+            for i in range(samples_ratio):
+                global j;j=(i+2)
+                newGroup=group_copy.copy()
+                newGroup=newGroup.apply(resample_signal,axis=1).reset_index(drop=True)
+                group=pd.concat([group,newGroup],axis=0)
+            return group
+        dataframe=dataframe.groupby(187).apply(oversample).reset_index(drop=True)
+
+    dataframe=dataframe.sample(frac=1)
+    y = dataframe[dataframe.columns[-1:]]
+    x = dataframe[dataframe.columns[:-1]]
+    y = y.to_numpy()
+    x = x.to_numpy()
+
+    return x,x_test,y,y_test
